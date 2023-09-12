7522c226baa74ab0d303c57f230dda6cf9fe2945
SafariBookingsHub <138075497+SafariBookingsHub@users.noreply.github.com>
2023-09-12 03:33:44 +0200

    Update PE.cpp

--- sourcecode/ProcessHollowing/PE.cpp
+++ sourcecode/ProcessHollowing/PE.cpp
@@ -3,93 +3,71 @@
 #include "internals.h"
 #include "pe.h"
 
+HMODULE hNTDLL = nullptr;
+_NtQueryInformationProcess ntQueryInformationProcess = nullptr;
+
+bool InitializeNtQueryInformationProcess()
+{
+    hNTDLL = LoadLibraryA("ntdll");
+    if (!hNTDLL)
+        return false;
+
+    FARPROC fpNtQueryInformationProcess = GetProcAddress(hNTDLL, "NtQueryInformationProcess");
+    if (!fpNtQueryInformationProcess)
+        return false;
+
+    ntQueryInformationProcess = (_NtQueryInformationProcess)fpNtQueryInformationProcess;
+    return true;
+}
+
 DWORD FindRemotePEB(HANDLE hProcess)
 {
-	HMODULE hNTDLL = LoadLibraryA("ntdll");
+    if(!ntQueryInformationProcess)
+    {
+        if(!InitializeNtQueryInformationProcess())
+            return 0;
+    }
 
-	if (!hNTDLL)
-		return 0;
+    PROCESS_BASIC_INFORMATION basicInfo = {0};
+    DWORD dwReturnLength = 0;
 
-	FARPROC fpNtQueryInformationProcess = GetProcAddress
-		(
-		hNTDLL,
-		"NtQueryInformationProcess"
-		);
-
-	if (!fpNtQueryInformationProcess)
-		return 0;
-
-	_NtQueryInformationProcess ntQueryInformationProcess = 
-		(_NtQueryInformationProcess)fpNtQueryInformationProcess;
-
-	PROCESS_BASIC_INFORMATION* pBasicInfo = 
-		new PROCESS_BASIC_INFORMATION();
-
-	DWORD dwReturnLength = 0;
-
-	ntQueryInformationProcess
-		(
-		hProcess, 
-		0, 
-		pBasicInfo, 
-		sizeof(PROCESS_BASIC_INFORMATION), 
-		&dwReturnLength
-		);
-
-	return pBasicInfo->PebBaseAddress;
+    ntQueryInformationProcess(hProcess, 0, &basicInfo, sizeof(basicInfo), &dwReturnLength);
+    return basicInfo.PebBaseAddress;
 }
 
 PEB* ReadRemotePEB(HANDLE hProcess)
 {
-	DWORD dwPEBAddress = FindRemotePEB(hProcess);
+    DWORD dwPEBAddress = FindRemotePEB(hProcess);
+    if(!dwPEBAddress)
+        return nullptr;
 
-	PEB* pPEB = new PEB();
+    PEB* pPEB = new PEB();
 
-	BOOL bSuccess = ReadProcessMemory
-		(
-		hProcess,
-		(LPCVOID)dwPEBAddress,
-		pPEB,
-		sizeof(PEB),
-		0
-		);
+    if(!ReadProcessMemory(hProcess, (LPCVOID)dwPEBAddress, pPEB, sizeof(PEB), nullptr))
+    {
+        delete pPEB;
+        return nullptr;
+    }
 
-	if (!bSuccess)
-		return 0;
-
-	return pPEB;
+    return pPEB;
 }
 
 PLOADED_IMAGE ReadRemoteImage(HANDLE hProcess, LPCVOID lpImageBaseAddress)
 {
-	BYTE* lpBuffer = new BYTE[BUFFER_SIZE];
+    BYTE* lpBuffer = new BYTE[BUFFER_SIZE];
+    if(!ReadProcessMemory(hProcess, lpImageBaseAddress, lpBuffer, BUFFER_SIZE, nullptr))
+    {
+        delete[] lpBuffer;
+        return nullptr;	
+    }
 
-	BOOL bSuccess = ReadProcessMemory
-		(
-		hProcess,
-		lpImageBaseAddress,
-		lpBuffer,
-		BUFFER_SIZE,
-		0
-		);
+    PIMAGE_DOS_HEADER pDOSHeader = (PIMAGE_DOS_HEADER)lpBuffer;
+    PLOADED_IMAGE pImage = new LOADED_IMAGE();
 
-	if (!bSuccess)
-		return 0;	
+    pImage->FileHeader = (PIMAGE_NT_HEADERS32)(lpBuffer + pDOSHeader->e_lfanew);
+    pImage->NumberOfSections = pImage->FileHeader->FileHeader.NumberOfSections;
+    pImage->Sections = (PIMAGE_SECTION_HEADER)(lpBuffer + pDOSHeader->e_lfanew + sizeof(IMAGE_NT_HEADERS32));
 
-	PIMAGE_DOS_HEADER pDOSHeader = (PIMAGE_DOS_HEADER)lpBuffer;
-
-	PLOADED_IMAGE pImage = new LOADED_IMAGE();
-
-	pImage->FileHeader = 
-		(PIMAGE_NT_HEADERS32)(lpBuffer + pDOSHeader->e_lfanew);
-
-	pImage->NumberOfSections = 
-		pImage->FileHeader->FileHeader.NumberOfSections;
-
-	pImage->Sections = 
-		(PIMAGE_SECTION_HEADER)(lpBuffer + pDOSHeader->e_lfanew + 
-		sizeof(IMAGE_NT_HEADERS32));
-
-	return pImage;
+    delete[] lpBuffer; // Avoid memory leak
+    return pImage;
 }
-
